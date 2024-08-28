d12f767b9fb583cd5527d9b8bc2b9291a52f135d
Omaga David <omagaowi@gmail.com>
2024-08-28 20:26:11 +0100

    bots

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ connectToDb((err) => {
     db = getDb();
     console.log("connected to database");
     if (env == "PRODUCTION") {
-      activateIntermdiateBot(db);
-      runDiscordBot(db);
+      // activateIntermdiateBot(db);
+      // runDiscordBot(db);
     }
     app.listen(4000);
   } else {
